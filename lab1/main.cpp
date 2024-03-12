6378406314985c33067b0cba9be9c4d73e1eb7e5
Itz-Verma <princekverma0702@gmail.com>
2024-03-12 18:03:57 +0530

    lab1 code added

---
+++ lab1/main.cpp
@@ -0,0 +1,20 @@
+#include <graphics.h>
+
+int main() {
+    int graphicsDriver = DETECT; // DETECT: macro which detect the graphic driver automatically.
+    
+    int graphicsMode;
+    
+    char* graphicsDriverDirectoryPath = NULL;
+
+    //initialize graph 
+    initgraph(&graphicsDriver, &graphicsMode, graphicsDriverDirectoryPath);
+    
+    //stop all process for 1000ms.
+    delay(1000);
+
+    
+    closegraph();
+
+    return 0;
+}
